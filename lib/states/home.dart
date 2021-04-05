6cfb3846cf8fcc36ac9489b31efe2742c59784e3
Wanish Sengkhim <wanish45037.2@gmail.com>
2021-04-05 23:39:49 +0700

    Wrap Expanded layout

--- lib/states/home.dart
+++ lib/states/home.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web/widgets/calltoaction.dart';
+import 'package:flutter_web/widgets/content.dart';
+import 'package:flutter_web/widgets/navigate.dart';
 import 'package:responsive_builder/responsive_builder.dart';
 
 class Home extends StatefulWidget {
@@ -10,12 +13,39 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ScreenTypeLayout(
-        desktop: Text('This is Home from Desktop'),
-        tablet: Text('This is Home from Tablet'),
-        mobile: Text('This is Home from Mobile'),
-        watch: Text('This is Home from Watch')
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(maxWidth: 1366),
+          child: ScreenTypeLayout(
+            desktop: buildDesktop(),
+            tablet: buildTabletAndMobile(),
+            mobile: buildTabletAndMobile()
+          ),
+        ),
       ),
     );
   }
+
+  Widget buildTabletAndMobile() => Column(
+    children: [
+      NavigatorLayout(),
+      CalltoActionLayout(),
+      ContentLayout()
+    ],
+  );
+
+  Widget buildDesktop() => Column(
+    children: [
+      NavigatorLayout(),
+      Expanded(
+        child: Row(
+          children: [
+            ContentLayout(),
+            CalltoActionLayout(),
+          ],
+        ),
+      ),
+    ],
+  );
 }
